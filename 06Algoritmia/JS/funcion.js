d742ef9a6d2403b67e61fc98189e543b78525091
MolinaGarciaAlexanderNicolas <amolinag2301@alumno.ipn.mx>
2025-10-25 13:22:21 -0600

    Se agrega el problema dos de algoritmia

--- 06Algoritmia/JS/funcion.js
+++ 06Algoritmia/JS/funcion.js
@@ -8,6 +8,33 @@ function problema1(){
 
 function problema2(){
     //jimmy
+    var p2_x1 = document.querySelector("#p2_x1").value;
+    var p2_x2 = document.querySelector("#p2_x2").value;
+    var p2_x3 = document.querySelector("#p2_x3").value;
+    var p2_x4 = document.querySelector("#p2_x4").value;
+    var p2_x5 = document.querySelector("#p2_x5").value;
+
+    var p2_y1 = document.querySelector("#p2_y1").value;
+    var p2_y2 = document.querySelector("#p2_y2").value;
+    var p2_y3 = document.querySelector("#p2_y3").value;
+    var p2_y4 = document.querySelector("#p2_y4").value;
+    var p2_y5 = document.querySelector("#p2_y5").value;
+
+    var v1 = [p2_x1, p2_x2, p2_x3, p2_x4, p2_x5];
+    var v1 = [p2_x1, p2_x2, p2_x3, p2_x4, p2_x5];
+
+    v1 = v1.sort(function(a, b){return b-a});
+    v2 = v2.sort(function(a, b){return b-a});
+
+    v2 = v2.reverse();
+
+    var p2_producto = 0;
+
+    for(var i = 0; i<v1; i++){
+        p2_producto == v1[i] * v2[i];
+    }
+
+    document.querySelector("#p2_resutado").textContent = "El producto escalar es minimo es: " + p2_producto;
 }
 
 function problema3(){
